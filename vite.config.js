e2af8ff2cfca44a98f85f4b408559e1cebaa0dd6
adamtoerner <adamtoerner@gmail.com>
2025-06-19 12:40:39 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ✅ Simplified config to let Vite auto-detect root and index.html
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
 })
