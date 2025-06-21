51e4e97f70adcb0250e5c0771d6b592b69ab734f
adamtoerner <adamtoerner@gmail.com>
2025-06-20 19:35:43 -0500

    up

--- src/utilities/schoolDistrictLookup.js
+++ src/utilities/schoolDistrictLookup.js
@@ -1,6 +1,10 @@
-// src/utilities/schoolDistrictLookup.js
-export async function getSchoolDistrict(lat, lon) {
-  const url = `https://services1.arcgis.com/0MSEUqKaxRlEPjAJ/arcgis/rest/services/School_District_Boundaries_Current/FeatureServer/0/query?where=1%3D1&geometry=${lon}%2C${lat}&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outFields=NAME&returnGeometry=false&f=json`;
+export async function getSchoolDistrict(latitude, longitude) {
+  if (!latitude || !longitude) {
+    console.error("Latitude and longitude must be provided.");
+    return null;
+  }
+
+  const url = `https://services1.arcgis.com/0MSEUqKaxRlEPjAJ/arcgis/rest/services/School_District_Boundaries_Current/FeatureServer/0/query?where=1%3D1&geometry=${longitude}%2C${latitude}&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outFields=NAME&returnGeometry=false&f=json`;
 
   try {
     const response = await fetch(url);
